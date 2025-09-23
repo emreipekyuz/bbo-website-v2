f3bc97220ec63451c5600873caae9943bf5b10a3
emreipekyuz <emreipekyuz@gmail.com>
2025-09-23 10:23:47 +0300

    layout.tsx güncellendi: metadataBase ve OG image ayarlandı

--- src/app/ekibimiz/page.tsx
+++ src/app/ekibimiz/page.tsx
@@ -14,6 +14,8 @@ function SectionTitle({ children }: { children: React.ReactNode }) {
 }
 
 export default function EkibimizPage() {
+  const IMG = "/team/melsa-bedir.png"; // 🔹 Tüm kartlarda kullanılacak ortak görsel
+
   return (
     <div className="bg-[#0e0f12] text-white">
       <div className="max-w-7xl mx-auto px-4 pt-10 pb-20">
@@ -23,16 +25,16 @@ export default function EkibimizPage() {
         <section className="mt-10">
           <SectionTitle>Yönetim Kurulu</SectionTitle>
           <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6">
-            <TeamCard name="Emre İpekyüz" role="Yönetim Kurulu Üyesi" src="/team/emre-ipekyuz.jpg" />
-            <TeamCard name="Musa İpekten" role="Yönetim Kurulu Üyesi" src="/team/musa-ipekten.jpg" />
-            <TeamCard name="Yağmur Akın" role="Yönetim Kurulu Üyesi" src="/team/yagmur-akin.jpg" />
-            <TeamCard name="Maysa Ünal" role="Yönetim Kurulu Üyesi" src="/team/maysa-unal.jpg" />
-            <TeamCard name="Nilsu Uysal" role="Yönetim Kurulu Üyesi" src="/team/nilsu-uysal.jpg" />
-            <TeamCard name="Gülsu Erdan" role="Yönetim Kurulu Üyesi" src="/team/gulsu-erdan.jpg" />
-            <TeamCard name="Aleyna Koldaş" role="Yönetim Kurulu Üyesi" src="/team/aleyna-koldas.jpg" />
-            <TeamCard name="Zeynep İshakoğlu" role="Yönetim Kurulu Üyesi" src="/team/zeynep-ishakoglu.jpg" />
-            <TeamCard name="Gülbahar Yüceyüksel" role="Yönetim Kurulu Üyesi" src="/team/gulbahar-yuceyuksel.jpg" />
-            <TeamCard name="Zeynep Sunay" role="Yönetim Kurulu Üyesi" src="/team/zeynep-sunay.jpg" />
+            <TeamCard name="Emre İpekyüz" role="Yönetim Kurulu Üyesi" src={IMG} />
+            <TeamCard name="Musa İpekten" role="Yönetim Kurulu Üyesi" src={IMG} />
+            <TeamCard name="Yağmur Akın" role="Yönetim Kurulu Üyesi" src={IMG} />
+            <TeamCard name="Maysa Ünal" role="Yönetim Kurulu Üyesi" src={IMG} />
+            <TeamCard name="Nilsu Uysal" role="Yönetim Kurulu Üyesi" src={IMG} />
+            <TeamCard name="Gülsu Erdan" role="Yönetim Kurulu Üyesi" src={IMG} />
+            <TeamCard name="Aleyna Koldaş" role="Yönetim Kurulu Üyesi" src={IMG} />
+            <TeamCard name="Zeynep İshakoğlu" role="Yönetim Kurulu Üyesi" src={IMG} />
+            <TeamCard name="Gülbahar Yüceyüksel" role="Yönetim Kurulu Üyesi" src={IMG} />
+            <TeamCard name="Zeynep Sunay" role="Yönetim Kurulu Üyesi" src={IMG} />
           </div>
         </section>
 
@@ -41,31 +43,31 @@ export default function EkibimizPage() {
           <SectionTitle>Komite Koordinatörleri</SectionTitle>
           <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6">
             {/* Eşitlik */}
-            <TeamCard name="Yağmur Akın" role="Eşitlik Komitesi Koordinatörü" />
-            <TeamCard name="Zeynep Sunay" role="Eşitlik Komitesi Koordinatörü" />
+            <TeamCard name="Yağmur Akın" role="Eşitlik Komitesi Koordinatörü" src={IMG} />
+            <TeamCard name="Zeynep Sunay" role="Eşitlik Komitesi Koordinatörü" src={IMG} />
 
             {/* Gençlik */}
-            <TeamCard name="Zehra Melek Tonka" role="Gençlik Komitesi Koordinatörü" />
-            <TeamCard name="Ayşegül Sağdıç" role="Gençlik Komitesi Koordinatörü" />
-            <TeamCard name="Nilsu Uysal" role="Gençlik Komitesi Koordinatörü" />
+            <TeamCard name="Zehra Melek Tonka" role="Gençlik Komitesi Koordinatörü" src={IMG} />
+            <TeamCard name="Ayşegül Sağdıç" role="Gençlik Komitesi Koordinatörü" src={IMG} />
+            <TeamCard name="Nilsu Uysal" role="Gençlik Komitesi Koordinatörü" src={IMG} />
 
             {/* Kültür Sanat */}
-            <TeamCard name="Maysa Ünal" role="Kültür Sanat Komitesi Koordinatörü" />
-            <TeamCard name="Kübra Rüya Yıldırım" role="Kültür Sanat Komitesi Koordinatörü" />
-            <TeamCard name="Kardelen Kopmaz" role="Kültür Sanat Komitesi Koordinatörü" />
-            <TeamCard name="İpek Bayık" role="Kültür Sanat Komitesi Koordinatörü" />
-            <TeamCard name="Gülbahar Yüceyüksel" role="Kültür Sanat Komitesi Koordinatörü" />
+            <TeamCard name="Maysa Ünal" role="Kültür Sanat Komitesi Koordinatörü" src={IMG} />
+            <TeamCard name="Kübra Rüya Yıldırım" role="Kültür Sanat Komitesi Koordinatörü" src={IMG} />
+            <TeamCard name="Kardelen Kopmaz" role="Kültür Sanat Komitesi Koordinatörü" src={IMG} />
+            <TeamCard name="İpek Bayık" role="Kültür Sanat Komitesi Koordinatörü" src={IMG} />
+            <TeamCard name="Gülbahar Yüceyüksel" role="Kültür Sanat Komitesi Koordinatörü" src={IMG} />
 
             {/* Sosyal Etki */}
-            <TeamCard name="Esra Gül Arslan" role="Sosyal Etki Komitesi Koordinatörü" />
-            <TeamCard name="Gözde Nur Kılınç" role="Sosyal Etki Komitesi Koordinatörü" />
-            <TeamCard name="Umut Eylül Alan" role="Sosyal Etki Komitesi Koordinatörü" />
-            <TeamCard name="Gülsu Erdan" role="Sosyal Etki Komitesi Koordinatörü" />
-            <TeamCard name="Aleyna Koldaş" role="Sosyal Etki Komitesi Koordinatörü" />
+            <TeamCard name="Esra Gül Arslan" role="Sosyal Etki Komitesi Koordinatörü" src={IMG} />
+            <TeamCard name="Gözde Nur Kılınç" role="Sosyal Etki Komitesi Koordinatörü" src={IMG} />
+            <TeamCard name="Umut Eylül Alan" role="Sosyal Etki Komitesi Koordinatörü" src={IMG} />
+            <TeamCard name="Gülsu Erdan" role="Sosyal Etki Komitesi Koordinatörü" src={IMG} />
+            <TeamCard name="Aleyna Koldaş" role="Sosyal Etki Komitesi Koordinatörü" src={IMG} />
 
             {/* Boş slotlar */}
             {Array.from({ length: 5 }).map((_, i) => (
-              <TeamCard key={i} name={`Komite Koordinatörü ${i + 16}`} role="Koordinatör" />
+              <TeamCard key={i} name={`Komite Koordinatörü ${i + 16}`} role="Koordinatör" src={IMG} />
             ))}
           </div>
         </section>
@@ -74,13 +76,13 @@ export default function EkibimizPage() {
         <section className="mt-16">
           <SectionTitle>Nilüfer Gençlik Programı</SectionTitle>
           <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-7 gap-6">
-            <TeamCard name="Melsa Bedir" role="Program Koordinatörü" src="/team/melsa-bedir.jpg" />
-            <TeamCard name="Emre İpekyüz" role="Program Koordinatörü" src="/team/emre-ipekyuz.jpg" />
-            <TeamCard name="Nehir Naz Yılmaz" role="Proje Koordinatörü" />
-            <TeamCard name="Şevval Güleç" role="Proje Koordinatörü" />
-            <TeamCard name="Beyza Kaya" role="Proje Koordinatörü" />
-            <TeamCard name="Dilek Öncü" role="Proje Koordinatörü" />
-            <TeamCard name="Nazrin Sanani" role="Proje Koordinatörü" />
+            <TeamCard name="Melsa Bedir" role="Program Koordinatörü" src={IMG} />
+            <TeamCard name="Emre İpekyüz" role="Program Koordinatörü" src={IMG} />
+            <TeamCard name="Nehir Naz Yılmaz" role="Proje Koordinatörü" src={IMG} />
+            <TeamCard name="Şevval Güleç" role="Proje Koordinatörü" src={IMG} />
+            <TeamCard name="Beyza Kaya" role="Proje Koordinatörü" src={IMG} />
+            <TeamCard name="Dilek Öncü" role="Proje Koordinatörü" src={IMG} />
+            <TeamCard name="Nazrin Sanani" role="Proje Koordinatörü" src={IMG} />
           </div>
         </section>
       </div>
